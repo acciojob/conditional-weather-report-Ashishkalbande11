4b1f548466d757e29514f9105c94544a6f98a99f
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-10-22 14:56:47 +0530

    cng

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Weather from "./Weather";
+import Weather from "./WeatherDisplay";
 
-const def = {temp : 0, conditions : "cool"}
+const def = {temperature : 0, conditions : "cool"}
 const App = () => {
   const [data, setData] = useState(def);
 
   useEffect(()=>{
     setTimeout(()=>{
-      setData({temp:25, conditions: "Sunny"})
+      setData({temperature : 25, conditions : "Sunny"})
     }, 1000)
   })
   return (
