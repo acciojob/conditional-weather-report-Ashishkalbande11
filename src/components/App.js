a22752577c79ae9e89d31d8159b13573dc475950
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-10-22 14:11:55 +0530

    wheater

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Weather from "./Weather";
 
+const def = {temp : 0, conditions : "cool"}
 const App = () => {
+  const [data, setData] = useState(def);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setData({temp:25, conditions: "Sunny"})
+    }, 1000)
+  })
   return (
     <div>
         {/* Do not remove the main div */}
+        <Weather prop={data}/>
     </div>
   )
 }
